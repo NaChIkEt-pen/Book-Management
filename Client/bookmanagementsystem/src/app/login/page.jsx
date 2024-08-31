965919efd4b52f75cda30f9986b06babc7345f25
NaChIkEt-pen <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-08-31 19:55:28 +0530

    updates

---
+++ Client/bookmanagementsystem/src/app/login/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { InputFields } from "./InputFields";
+
+const page = () => {
+  return <div>{<InputFields />}</div>;
+};
+
+export default page;
--- Client/bookmanagementsystem/src/app/page.js
+++ Client/bookmanagementsystem/src/app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <NavBar />;
 }
