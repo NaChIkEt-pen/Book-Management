42a4cd2f2784a8494ad7f530683ca9c90c15d7e6
NaChIkEt-pen <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-09-06 22:34:03 +0530

    updates

--- Client/bookmanagementsystem/src/app/components/DashBoard/BookShowCase.jsx
+++ Client/bookmanagementsystem/src/app/components/DashBoard/BookShowCase.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import Image from "next/image";
 
 function BookShowCase() {
-  return <div>BookShowCase</div>;
+  return (
+    <div className="flex-col justify-end w-full">
+      <div className="flex-col justify-end w-full">
+        <div className="flex justify-end w-full pr-20 pt-10">
+          <Image
+            src="/BookImages/Dune1.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+          <Image
+            src="/BookImages/Dune2.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+          <Image
+            src="/BookImages/HP1.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+        </div>
+        <div className="flex justify-end w-full">
+          <Image
+            src="/shelf.png"
+            alt="shelf"
+            width={670}
+            height={100}
+            className={``}
+          />
+        </div>
+      </div>
+      <div className="flex-col justify-end w-full">
+        <div className="flex justify-end w-full pr-20 pt-10">
+          <Image
+            src="/BookImages/HP2.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+          <Image
+            src="/BookImages/HP3.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+          <Image
+            src="/BookImages/HP4.jpg"
+            alt="shelf"
+            width={110}
+            height={10}
+            className={`mr-12`}
+          />
+        </div>
+        <div className="flex justify-end w-full">
+          <Image
+            src="/shelf.png"
+            alt="shelf"
+            width={670}
+            height={100}
+            className={``}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookShowCase;
