d563b7f689d3ca4aeb98e5a0a24444142028cd22
NaChIkEt-pen <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-09-06 22:57:08 +0530

    updates

--- Client/bookmanagementsystem/src/app/components/DashBoard/BookShowCase.jsx
+++ Client/bookmanagementsystem/src/app/components/DashBoard/BookShowCase.jsx
@@ -2,74 +2,86 @@ import React from "react";
 import Image from "next/image";
 
 function BookShowCase() {
+  let previewUrl = "/BookImages/Dune1.jpg";
   return (
-    <div className="flex-col justify-end w-full">
-      <div className="flex-col justify-end w-full">
-        <div className="flex justify-end w-full pr-20 pt-10">
-          <Image
-            src="/BookImages/Dune1.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
-          <Image
-            src="/BookImages/Dune2.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
-          <Image
-            src="/BookImages/HP1.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
-        </div>
-        <div className="flex justify-end w-full">
-          <Image
-            src="/shelf.png"
-            alt="shelf"
-            width={670}
-            height={100}
-            className={``}
-          />
-        </div>
+    <div className="flex justify-end w-full">
+      <div className="PreviewDiv pl-20">
+        <Image
+          src={previewUrl}
+          alt="shelf"
+          width={450}
+          height={11}
+          className={`mr-12`}
+        />
       </div>
       <div className="flex-col justify-end w-full">
-        <div className="flex justify-end w-full pr-20 pt-10">
-          <Image
-            src="/BookImages/HP2.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
-          <Image
-            src="/BookImages/HP3.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
-          <Image
-            src="/BookImages/HP4.jpg"
-            alt="shelf"
-            width={110}
-            height={10}
-            className={`mr-12`}
-          />
+        <div className="flex-col justify-end w-full">
+          <div className="flex justify-end w-full pr-20 pt-10">
+            <Image
+              src="/BookImages/Dune1.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+            <Image
+              src="/BookImages/Dune2.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+            <Image
+              src="/BookImages/HP1.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+          </div>
+          <div className="flex justify-end w-full">
+            <Image
+              src="/shelf.png"
+              alt="shelf"
+              width={670}
+              height={100}
+              className={``}
+            />
+          </div>
         </div>
-        <div className="flex justify-end w-full">
-          <Image
-            src="/shelf.png"
-            alt="shelf"
-            width={670}
-            height={100}
-            className={``}
-          />
+        <div className="flex-col justify-end w-full">
+          <div className="flex justify-end w-full pr-20 pt-10">
+            <Image
+              src="/BookImages/HP2.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+            <Image
+              src="/BookImages/HP3.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+            <Image
+              src="/BookImages/HP4.jpg"
+              alt="shelf"
+              width={110}
+              height={10}
+              className={`mr-12`}
+            />
+          </div>
+          <div className="flex justify-end w-full">
+            <Image
+              src="/shelf.png"
+              alt="shelf"
+              width={670}
+              height={100}
+              className={``}
+            />
+          </div>
         </div>
       </div>
     </div>
